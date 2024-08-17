c96ffd75ebed0c8422a516f892e0215752295f07
Tinashe Wamambo <tinashe.wamambo@hotmail.co.uk>
2024-08-17 21:34:25 +0100

    Add endpoint to store new weather data

--- ocula/weatherapp/viewsets.py
+++ ocula/weatherapp/viewsets.py
@@ -6,6 +6,10 @@ from ocula.weatherapp.models import Temperature, City
 
 from datetime import datetime
 
+import requests
+
+from django.conf import settings
+
 
 class TemperatureSerializer(serializers.Serializer):
     minimum = serializers.FloatField()
@@ -31,4 +35,42 @@ class TemperatureViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def create(self, request):
-        pass
+        try:
+            city = City.objects.get(name__iexact=request.POST['city'])
+        except City.DoesNotExist:
+            return Response({'message': 'Invalid city! Please enter a valid UK city'}, status=404)
+
+        recorded_at_date = datetime.strptime(request.POST['date'], '%Y-%m-%d')
+
+        # retrieve city coordinates from Weather API
+        if not any([city.longitude, city.latitude]):
+            response = requests.get(
+                "http://api.openweathermap.org/geo/1.0/direct?"
+                f"q={city.name},GB&appid={settings.WEATHER_API_KEY}"
+            )
+            response_json = response.json()
+
+            city.latitude = response_json[0]["lat"]
+            city.longitude = response_json[0]["lon"]
+            city.save()
+
+        # retrieve temperate data from Weather API
+        timestamp = int(recorded_at_date.timestamp())
+        response = requests.get(
+            "https://history.openweathermap.org/data/2.5/history/city?"
+            f"lat={city.latitude}&lon={city.longitude}&type=hour&start={timestamp}&"
+            f"end={timestamp}&appid={settings.WEATHER_API_KEY}"
+        )
+        response_json = response.json()
+
+        temperature = Temperature.objects.create(
+            minimum=response_json["list"][0]["main"]["temp_min"],
+            maximum=response_json["list"][0]["main"]["temp_max"],
+            average=response_json["list"][0]["main"]["temp"],
+            humidity=response_json["list"][0]["main"]["humidity"],
+            city=city,
+            recorded_at=recorded_at_date,
+        )
+        serializer = TemperatureSerializer(temperature)
+
+        return Response(serializer.data)
